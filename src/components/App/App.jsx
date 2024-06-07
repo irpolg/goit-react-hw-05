3f96887dd3d77cc662772446a350fc35a99dc814
Iryna P <irpolg@ukr.net>
2024-06-07 21:43:52 +0300

    5hw-3app

---
+++ src/components/App/App.jsx
@@ -0,0 +1,33 @@
+import './App.module.css';
+import { Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+import Navigation from '../Navigation/Navigation';
+
+const HomePage = lazy(() => import('../../pages/HomePage/HomePage'));
+const MoviesPage = lazy(() => import('../../pages/MoviesPage/MoviesPage'));
+const MovieDetailsPage = lazy(() => import('../../pages/MovieDetailsPage/MovieDetailsPage'));
+const MovieCast = lazy(() => import('../MovieCast/MovieCast'));
+const MovieReviews = lazy(() => import('../MovieReviews/MovieReviews'));
+import NotFoundPage from '../../pages/NotFoundPage/NotFoundPage';
+
+const App = () => {
+    return (
+        <div>
+            <Navigation />
+            <Suspense fallback={null}>  
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/movies" element={<MoviesPage />} />
+                    <Route path="/movies/:movieId" element={<MovieDetailsPage />}> 
+                        {/* '/movies/:movieId/cast'     /movies/:movieId/reviews  */}
+                        <Route path='cast' element={<MovieCast/>} />
+                        <Route path='reviews' element={<MovieReviews />} />
+                    </Route>
+                    <Route path="*" element={<NotFoundPage />} />
+                </Routes>
+            </Suspense>
+        </div>
+  );
+}
+
+export default App;
---
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -0,0 +1,16 @@
+// Slack - mentor
+//Під час сабміту форми на сторінці MoviesPage 
+//обов’язково записувати слово для пошуку в параметри рядка запиту, 
+//сама форма повинна бути в окремому компоненті.
+//const handleSubmit = value => {
+// setSearchParams({ query: value });
+// };
+
+
+//На сторінках MoviesPage, MovieCast, MovieReviews
+//в масиві залежностей повинен бути id фільма який 
+//ми отримали з рядка запиту
+// const { movieId } = useParams();
+// useEffect(() => {
+// if (!movieId) return;
+// }, [movieId]);
